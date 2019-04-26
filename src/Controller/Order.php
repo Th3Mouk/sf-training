d1da1761e5bd8e425c89d87b5df4345447e48c2b
vichapon <vincent.chapon@etu.uca.fr>
2019-04-26 14:09:08 +0200

    ajout commentaire

--- src/Controller/Order.php
+++ src/Controller/Order.php
@@ -17,8 +17,8 @@ final class Order
         //todo
         $orders = FakeDB::getOrders();
       //  $ArrayOrders = new array();         //je laisse la preuve de mes essais en commentaire
-        foreach ($orders as $order) {
-          $ArrayOrders[] = $order->toArray();
+        foreach ($orders as $order) {   // je fait un foreach pour pouvoir récuperer des objets unique et appeller des méthodes sur chacun d'entre eux
+          $ArrayOrders[] = $order->toArray(); // appel de la fonction toArray pour que JsonResponse les affiches correctement
         }
         return new JsonResponse($ArrayOrders);
     }
@@ -28,7 +28,7 @@ final class Order
         //todo
         $orders = FakeDB::getOrders();
         foreach ($orders as $order) {
-            if($order->status()=="paid")
+            if($order->status()=="paid") // je compare leurs états. si ils sont "paid" je les ajoutes
               $ArrayOrdersPaid[] = $order->toArray();
           }
         return new JsonResponse($ArrayOrdersPaid);
@@ -39,7 +39,7 @@ final class Order
         //todo
         $orders = FakeDB::getOrders();
         foreach ($orders as $order) {
-          if(strpos($order->client_email(), $name) !== false)
+          if(strpos($order->client_email(), $name) !== false) // comparaison pour savoir si l'adresse mail contient le nom envoyé en paramètre.
             $ArrayOrders[] = $order->toArray();
           }
         return new JsonResponse($ArrayOrders);
@@ -55,19 +55,15 @@ final class Order
     public function getLegacy()
     {
       $orders = FakeDB::getOrders();
-      $oldorders = LegacyDB::getOrders();
+      $oldorders = LegacyDB::getOrders(); // je charge l'ancienne bdd
       foreach ($orders as $order) {
         $ArrayOrders[] = $order->toArray();
-        $ArrayUuid[] = $order->uuid();
+        $ArrayUuid[] = $order->uuid(); // je stocke l'uuid des nouveaux "order" afin de les comparer plus tard.
       }
       foreach ($oldorders as $oldorder) {
-        if(!in_array($oldorder->uuid(), $ArrayUuid))
+        if(!in_array($oldorder->uuid(), $ArrayUuid)) // si l'uuid n'est pas dans le tableau de nouveaux orders, je l'ajoute
           $ArrayOrders[] = $oldorder->toArray();
       }
-
-
-
-
       return new JsonResponse($ArrayOrders);
     }
 }
